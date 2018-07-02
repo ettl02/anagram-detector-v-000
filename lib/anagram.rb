fa45a432f4fc55d6edc8045d5e9639255bb80804
Emily Wessel <ettl02@gmail.com>
2018-07-02 18:42:59 +0000

    automatically backed up by learn

--- lib/anagram.rb
+++ lib/anagram.rb
@@ -8,7 +8,7 @@ def initialize (word)
 end
 
 def match (array)
-  array.select {|x|x.split("").sort} == @name.split("").sort}
+  array.select {|x|x.split("").sort == @name.split("").sort}
 
 end
 end
